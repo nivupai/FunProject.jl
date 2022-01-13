18e378c1cb6e22f5cf3e4003d7d75eccdb1d7638
Nivedita Rethnakar <n.paipriv@gmail.com>
2022-01-12 22:12:55 -0800

    Combinatorics added and a toy function

--- src/FunProject.jl
+++ src/FunProject.jl
@@ -1,4 +1,34 @@
 module FunProject
+using Combinatorics
+
+"""
+Each word in a sentence (string) is sorted alphabetically.
+
+#### Examples
+```julia-repl
+julia> order_string("This is a sorted sentence; Who is 1 and two  ")
+" This is a deorst ;ceeennst Who is 1 adn otw  "
+
+```
+"""
+function order_string(s::AbstractString="Hello world")
+	
+	s0=replace(s,"+"=>" + ","-"=>" - ")
+	x=split(s0," ") # Split into words delim=" "
+	
+	Y=""
+	for wIdx = 1:length(x)
+		# w=x[wIdx] # word
+		y=string(sort([a for a in x[wIdx]])...)  # sort each word
+		Y= Y * " " * y 
+	end
+	Yout=replace(Y," " =>" ")
+	
+	return Yout
+end
+
+export order_string
+
 
 # Write your package code here.
 greet() = "Hello World"
