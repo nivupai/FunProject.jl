ea56b59d34badd7eceec274881bb92ac90bf58e4
Documenter.jl <documenter@juliadocs.github.io>
2022-01-13 05:50:16 +0000

    build based on 188c826

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FunProject","category":"page"},{"location":"#FunProject","page":"Home","title":"FunProject","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FunProject.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FunProject]","category":"page"}]
+}
