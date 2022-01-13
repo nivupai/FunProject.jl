fead6a0e4cd9e9e4cc4912dbabc3554d74f8d4e8
Nivedita Rethnakar <n.paipriv@gmail.com>
2022-01-12 21:57:51 -0800

    Basic stuffs added for docs

--- docs/src/index.md
+++ docs/src/index.md
@@ -3,6 +3,8 @@ CurrentModule = FunProject
 ```
 
 # FunProject
+## Sub section bla
+Here we are going to explain all the nuances.
 
 Documentation for [FunProject](https://github.com/nivupai/FunProject.jl).
 
--- src/FunProject.jl
+++ src/FunProject.jl
@@ -1,5 +1,21 @@
 module FunProject
 
 # Write your package code here.
+greet() = "Hello World"
+export greet
+
+"""
+Adding two functions
+```julia-repl
+julia> add_two(3,2)
+
+```
+More bla
+"""
+function add_two(x,y)
+    return x+y
+end
+
+export add_two
 
 end
